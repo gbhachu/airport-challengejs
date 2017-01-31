edc6d70b21cb6a4abd2f41d0ea4a16b444e04fac
Matthew Perry <thesedatedprince@gmail.com>
2017-01-31 15:46:13 +0000

    Updated so weather prevents takeoff and landing

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,25 +4,55 @@ describe('Airport', function() {
 
     var airport;
     var plane;
+    var weather;
 
-    beforeEach (function(){ airport = new Airport();
+    beforeEach (function(){
       plane = new Plane();
-
+      weather = new Weather();
+      airport = new Airport(weather);
     });
 
-    describe('land plane at airport', function() {
-        it('lands a plane', function() {
-            airport.landPlane(plane);
-            expect(airport._planes).toContain(plane);
+    describe('weather is not stormy', function(){
+
+      beforeEach(function() {
+        spyOn(weather, 'isStormy').and.returnValue(false);
+      });
+
+      describe('land plane at airport', function() {
+          it('lands a plane', function() {
+              airport.landPlane(plane);
+              expect(airport._planes).toContain(plane);
+          });
+      });
+
+      describe('take off plane from airport', function(){
+        it('makes a plane takeoff', function(){
+          airport.landPlane(plane);
+          airport.takeoffPlane(plane);
+          expect(airport._planes).not.toContain(plane);
+        });
+      });
+    });
+
+    describe('weather is stormy', function(){
+      beforeEach(function() {
+        spyOn(weather, 'isStormy').and.returnValue(true);
+      });
+
+      describe('stops plane from taking off if stormy', function() {
+      it('prevents a plane from taking off', function(){
+        airport._planes.push(plane);
+        expect(function() {airport.takeoffPlane(plane)}).toThrowError("cannot take off: weather is stormy");
+      })
+    })
+
+    describe('stops plane from landing if stormy', function() {
+        it('raises cannot land plane error', function() {
+            expect(function() {airport.landPlane(plane)}).toThrowError("cannot land: weather is stormy");
         });
     });
 
-    describe('take off plane from airport', function(){
-      it('makes a plane takeoff', function(){
-        airport.landPlane(plane);
-        airport.takeoffPlane(plane);
-        expect(airport._planes).not.toContain(plane);
-      });
-    });
+  });
+
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,14 +1,23 @@
-function Airport(){
+function Airport(weather){
   this._planes = [];
+  this._weather= weather
   };
 
 Airport.prototype.landPlane = function(plane) {
-  this._planes.push(plane);
-  console.log('This plane has landed')
+  if(this._weather.isStormy() === true){
+    throw new Error("cannot land: weather is stormy")
+  } else {
+    this._planes.push(plane);
+    console.log('This plane has landed')
+  }
 };
 
 Airport.prototype.takeoffPlane = function(plane) {
+  if(this._weather.isStormy() === true){
+    throw new Error("cannot take off: weather is stormy")
+  } else {
   var index = this._planes.indexOf(plane);
   this._planes.splice(index, 1);
   console.log('This plane has taken off')
+  }
 }
